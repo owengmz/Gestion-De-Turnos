6bf8812f2270ecc16f82601565317b87314f7ad8
owengmz <heber12398@gmail.com>
2026-09-25 20:46:44 -0300

    reinicio

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Gestion_De_Turnos.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
